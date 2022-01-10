fa30ba2f546005898c5a98830d28213b68b0ee74
Joao <joaoheron@gmail.com>
2022-01-10 15:52:29 -0300

    Revert key properties modifications

--- tap_looker/streams.py
+++ tap_looker/streams.py
@@ -14,42 +14,42 @@
 
 STREAMS = {
     'color_collections': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'ColorCollection'
     },
     'connections': {
         'key_properties': ['name'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'DBConnection'
     },
     'dashboards': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'DashboardBase',
         'children': {
             'dashboard_elements': {
                 'path': 'dashboards/{}/dashboard_elements',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'DashboardElement',
                 'children': {
                     'queries': {
                         'path': 'queries/[query_id]',
-                        'key_properties': [],
-                        'replication_method': 'INCREMENTAL',
+                        'key_properties': ['id'],
+                        'replication_method': 'FULL_TABLE',
                         'swagger_object': 'Query'
                     },
                     'merge_queries': {
                         'path': 'merge_queries/[merge_result_id]',
-                        'key_properties': [],
-                        'replication_method': 'INCREMENTAL',
+                        'key_properties': ['id'],
+                        'replication_method': 'FULL_TABLE',
                         'swagger_object': 'MergeQuery',
                         'children': {
                             'queries': {
                                 'path': 'queries/[query_id]',
-                                'key_properties': [],
-                                'replication_method': 'INCREMENTAL',
+                                'key_properties': ['id'],
+                                'replication_method': 'FULL_TABLE',
                                 'swagger_object': 'Query'
                             }
                         }
@@ -58,132 +58,132 @@ STREAMS = {
             },
             'dashboard_filters': {
                 'path': 'dashboards/{}/dashboard_filters',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'DashboardFilter'
             },
             'dashboard_layouts': {
                 'path': 'dashboards/{}/dashboard_layouts',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'DashboardLayout'
             },
             'content_metadata': {
                 'path': 'content_metadata/[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'scheduled_plans': {
                 'path': 'scheduled_plans/dashboard/{}?all_users=true',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ScheduledPlan'
             }
         }
     },
     'datagroups': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Datagroup'
     },
     'folders': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Folder',
         'children': {
             'content_metadata': {
                 'path': 'content_metadata?parent_id=[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'content_metadata': {
                 'path': 'content_metadata/[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'content_metadata_access': {
                 'path': 'content_metadata_access?content_metadata_id=[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMetaGroupUser'
             }
         }
     },
     'groups': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Group',
         'children': {
             'groups_in_group': {
                 'path': 'groups/{}/groups',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'Group',
                 'parent': 'parent_group'
             }
         }
     },
     'homepages': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Homepage',
         'children': {
             'content_metadata': {
                 'path': 'content_metadata/[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'content_metadata_access': {
                 'path': 'content_metadata_access?content_metadata_id=[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMetaGroupUser'
             }
         }
     },
     'integration_hubs': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'IntegrationHub'
     },
     'integrations': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Integration'
     },
     'lookml_dashboards': {
         'path': 'dashboards',
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'DashboardBase',
         'children': {
             'scheduled_plans': {
                 'path': 'scheduled_plans/lookml_dashboard/{}?all_users=true',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ScheduledPlan'
             }
         }
     },
     'lookml_models': {
         'key_properties': ['name', 'project_name'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'LookmlModel',
         'children': {
             'models': {
                 'path': 'lookml_models/{}',
                 'key_properties': ['name', 'project_name'],
-                'replication_method': 'INCREMENTAL',
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'LookmlModel',
                 'children': {
                     'explores': {
                         'path': 'lookml_models/{}/explores/[child_id]',
-                        'key_properties': [],
-                        'replication_method': 'INCREMENTAL',
+                        'key_properties': ['id'],
+                        'replication_method': 'FULL_TABLE',
                         'swagger_object': 'LookmlModelExplore'
                     }
                 }
@@ -191,75 +191,75 @@ STREAMS = {
         }
     },
     'looks': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Look',
         'children': {
             'queries': {
                 'path': 'queries/[query_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'Query'
             },
             'content_metadata': {
                 'path': 'content_metadata/[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'scheduled_plans': {
                 'path': 'scheduled_plans/look/{}?all_users=true',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ScheduledPlan'
             }
         }
     },
     'model_sets': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'ModelSet'
     },
     'permission_sets': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'PermissionSet'
     },
     'permissions': {
         'key_properties': ['permission'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Permission'
     },
     'projects': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Project',
         'children': {
             'project_files': {
                 'path': 'projects/{}/files',
                 'key_properties': ['id', 'project_id'],
-                'replication_method': 'INCREMENTAL',
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ProjectFile',
                 'parent': 'project'
             },
             'git_branches': {
                 'path': 'projects/{}/git_branches',
                 'key_properties': ['name', 'project_id'],
-                'replication_method': 'INCREMENTAL',
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'GitBranch',
                 'parent': 'project'
             }
         }
     },
     'roles': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Role',
         'children': {
             'role_groups': {
                 'path': 'roles/{}/groups',
                 'key_properties': ['id', 'role_id'],
-                'replication_method': 'INCREMENTAL',
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'Group',
                 'parent': 'role'
             }
@@ -267,103 +267,103 @@ STREAMS = {
     },
     'scheduled_plans': {
         'path': 'scheduled_plans?all_users=true',
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'ScheduledPlan'
     },
     'spaces': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Space',
         'children': {
             'content_metadata': {
                 'path': 'content_metadata?parent_id=[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'content_metadata': {
                 'path': 'content_metadata/[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMeta'
             },
             'content_metadata_access': {
                 'path': 'content_metadata_access?content_metadata_id=[content_metadata_id]',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentMetaGroupUser'
             }
         }
     },
     'themes': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Theme'
     },
     'user_attributes': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'UserAttribute',
         'children': {
             'user_attribute_group_values': {
                 'path': 'user_attributes/{}/group_values',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'UserAttributeGroupValue'
             }
         }
     },
     'user_login_lockouts': {
         'key_properties': ['key'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'UserLoginLockout'
     },
     'users': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'User',
         'children': {
             'user_attribute_values': {
                 'path': 'users/{}/attribute_values?all_values=true&include_unset=true',
                 'key_properties': ['user_id', 'user_attribute_id'],
-                'replication_method': 'INCREMENTAL',
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'UserAttributeWithValue'
             },
             'user_sessions': {
                 'path': 'users/{}/sessions',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'Session'
             },
             'content_favorites': {
                 'path': 'content_favorite/search?user_id={}',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentFavorite'
             },
             'content_views': {
                 'path': 'content_view/search?user_id={}',
-                'key_properties': [],
-                'replication_method': 'INCREMENTAL',
+                'key_properties': ['id'],
+                'replication_method': 'FULL_TABLE',
                 'swagger_object': 'ContentView'
             }
         }
     },
     'versions': {
         'key_properties': ['looker_release_version'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'ApiVersion'
     },
     'workspaces': {
-        'key_properties': [],
-        'replication_method': 'INCREMENTAL',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
         'swagger_object': 'Workspace'
     },
     'query_history': {
         'path': 'queries/run/json?limit=10000&apply_formatting=false&apply_vis=false&cache=false&force_production=true&server_table_calcs=false',
         'key_properties': ['query_id', 'history_created_date', 'dims_hash_key'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'method': 'POST',
         'body': {
             'model': 'i__looker',
